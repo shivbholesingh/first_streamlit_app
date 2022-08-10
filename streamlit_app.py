8b2b5d83092f9c429062c3391c724711f0c2407f
shivbholesingh <34954928+shivbholesingh@users.noreply.github.com>
2022-08-10 08:26:47 +0530

    Update streamlit_app.py
    with Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega3 & Blueberry Oatmeal")
+streamlit.text('Kale, Spnich & Rocket Smoothie')
+stramit.text('Hard-Boiled Free-Range Egg')
