e623c117034a1e99ff97aff0c88432d28404b6e9
shivbholesingh <34954928+shivbholesingh@users.noreply.github.com>
2022-08-23 21:34:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+# streamlit.stop()
 
 
 streamlit.header("The Fruit load list contains:")
